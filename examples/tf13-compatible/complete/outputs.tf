65bdd7111a206fe7a76f1f34e35b3d3d19f71553
creynolds15 <colin.reynolds@tamr.com>
2024-01-03 13:53:07 -0800

    chore: outputs to sensitive

--- examples/tf13-compatible/complete/outputs.tf
+++ examples/tf13-compatible/complete/outputs.tf
@@ -30,4 +30,5 @@ output "emr" {
 
 output "tamr-config" {
   value = module.tamr-config.tamr_config_file
+  sensitive = true
 }
--- test_examples/root_module/outputs.tf
+++ test_examples/root_module/outputs.tf
@@ -1,3 +1,4 @@
 output "tamr-config" {
   value = module.tamr-config.tamr_config_file
+  sensitive = true
 }
